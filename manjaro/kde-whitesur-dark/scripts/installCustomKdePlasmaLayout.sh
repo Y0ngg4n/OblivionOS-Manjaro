2e3dceea2e43e2ac7f59d51a195c2b0f7e8e01d6
Yonggan <yonggan@oblivioncoding.pro>
2021-02-14 15:26:11 +0100

    Fixed Splash Logo, Try fix launchpad Plasma

--- manjaro/kde-whitesur-dark/scripts/installCustomKdePlasmaLayout.sh
+++ manjaro/kde-whitesur-dark/scripts/installCustomKdePlasmaLayout.sh
@@ -6,3 +6,11 @@ LOOKFEEL_DIR="${INSTALL_DIR}/usr/share/plasma/look-and-feel"
 cp -f org.kde.plasma.desktop-layout.js $LOOKFEEL_DIR/com.github.vinceliuice.WhiteSur/contents/layouts
 cp -f org.kde.plasma.desktop-layout.js $LOOKFEEL_DIR/com.github.vinceliuice.WhiteSur-alt/contents/layouts
 cp -f org.kde.plasma.desktop-layout.js $LOOKFEEL_DIR/com.github.vinceliuice.WhiteSur-dark/contents/layouts
+
+cp Splash.svg $LOOKFEEL_DIR/com.github.vinceliuice.WhiteSur/contents/splash/images/logo.svg
+cp Splash.svg $LOOKFEEL_DIR/com.github.vinceliuice.WhiteSur-alt/contents/splash/images/logo.svg
+cp Splash.svg $LOOKFEEL_DIR/com.github.vinceliuice.WhiteSur-dark/contents/splash/images/logo.svg
+
+cp Logo.svg $INSTALL_DIR/usr/share/icons/oblivion/Logo.svg
+cp Hole.svg $INSTALL_DIR/usr/share/icons/oblivion/Hole.svg
+cp Hole20.svg $INSTALL_DIR/usr/share/icons/oblivion/Hole20.svg
